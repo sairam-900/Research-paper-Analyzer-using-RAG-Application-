50786a8c9a01f897fda36a48df57728145d32312
NAKKA LEELA LAKSHMI SAI RAM <nleelasairamnakka@gmail.com>
2026-02-20 00:07:59 +0530

    Change app.run to use host and port parameters

--- main.py
+++ main.py
@@ -155,5 +155,6 @@ def chat_with_paper():
     return jsonify({"answer": answer_html})
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
